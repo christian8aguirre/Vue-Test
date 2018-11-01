e59bc1942436571499f12da7a8ce32860dc46134
chrishio <christian8.aguirre@icloud.com>
2018-11-01 17:32:16 -0600

    Reproductor YouTube 5

--- 08-reproductor-YouTube/01-reproductor-YouTube/vue.js
+++ 08-reproductor-YouTube/01-reproductor-YouTube/vue.js
@@ -1,13 +1,30 @@
-let vm = new ({
+let vm = new Vue({
     el: '#main',
     data: {
         laURL: '',
         elTitulo: '',
         videoActivo: '',
         mostrar: false,
-        video: [],
+        videos: [],
     },
     methods: {
-        
+        showTitulo: function(){
+            if(this.laURL!="")
+            this.mostrar=!this.mostrar;
+            setTimeout(function () { $('#ttl').focus();}, 100);
+        },
+        addDatos: function () {
+            if(this.elTitulo!='')
+            this.mostrar=!this.mostrar;
+            this.videos.push({ url: this.laURL, titulo: this.elTitulo, activo: false });
+            //Guarda el objeto como un string
+            LocalStorage.setItem('pistas', JSON.stringify(this.videos));
+            this.laURL = this.elTitulo = '';
+        },
+        playVideo: function (video) {
+            let laURL = video.url;
+            let nuevaUrl = laURL.replace('watch?v=', 'embed/');
+            this.videoActivo= nuevaUrl + '?&autoplay=1';
+        }
     }
 })
