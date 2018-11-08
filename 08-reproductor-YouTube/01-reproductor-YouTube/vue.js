d32daab98d407ddad4cbf78a0ca8f3e237dd01d4
chrishio <christian8.aguirre@icloud.com>
2018-11-08 16:25:27 -0600

    Parte 5 YouTube

--- 08-reproductor-YouTube/01-reproductor-YouTube/vue.js
+++ 08-reproductor-YouTube/01-reproductor-YouTube/vue.js
@@ -1,30 +1,42 @@
 let vm = new Vue({
-    el: '#main',
-    data: {
-        laURL: '',
-        elTitulo: '',
-        videoActivo: '',
-        mostrar: false,
-        videos: [],
+  el: "#main",
+  data: {
+    laURL: "",
+    elTitulo: "",
+    videoActivo: "",
+    mostrar: false,
+    videos: []
+  },
+  created() {
+    //Añade las pistas al objeto videos
+    this.videos = JSON.parse(localStorage.getItem("pistas"));
+  },
+  methods: {
+    showTitulo: function() {
+      if (this.laURL != "") this.mostrar = !this.mostrar;
+      setTimeout(function() {
+        $("#ttl").focus();
+      }, 100);
     },
-    methods: {
-        showTitulo: function(){
-            if(this.laURL!="")
-            this.mostrar=!this.mostrar;
-            setTimeout(function () { $('#ttl').focus();}, 100);
-        },
-        addDatos: function () {
-            if(this.elTitulo!='')
-            this.mostrar=!this.mostrar;
-            this.videos.push({ url: this.laURL, titulo: this.elTitulo, activo: false });
-            //Guarda el objeto como un string
-            LocalStorage.setItem('pistas', JSON.stringify(this.videos));
-            this.laURL = this.elTitulo = '';
-        },
-        playVideo: function (video) {
-            let laURL = video.url;
-            let nuevaUrl = laURL.replace('watch?v=', 'embed/');
-            this.videoActivo= nuevaUrl + '?&autoplay=1';
-        }
+    addDatos: function() {
+      if (this.elTitulo != "") this.mostrar = !this.mostrar;
+      this.videos.push({
+        url: this.laURL,
+        titulo: this.elTitulo,
+        activo: false
+      });
+      //Guarda el objeto como un string
+      localStorage.setItem("pistas", JSON.stringify(this.videos));
+      this.laURL = this.elTitulo = "";
+    },
+    playVideo: function(video) {
+      let laURL = video.url;
+      let nuevaUrl = laURL.replace("watch?v=", "embed/");
+      this.videoActivo = nuevaUrl + "?&autoplay=1";
+      for (i = 0; i < this.videos.length; i++) {
+        this.videos[i].activo = false;
+      }
+      video.activo = true;
     }
-})
+  }
+});
